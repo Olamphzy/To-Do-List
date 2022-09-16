51b67d40790430e67b65227fd77d2ef2898b07bb
Olamphzy <61218900+Olamphzy@users.noreply.github.com>
2022-09-16 07:59:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Text")
+    } 
+    else {
+        document.querySelector('#task').innerHTML 
+        += `
+        <div class='task'>
+           <span id="taskname">
+               ${document.querySelector("#newtask input").value}
+            </span>
+            <button class="delete">
+            <i class="far fa-trash-alt"></i>
+            </button>
+
+        </div>
+        `;
+
+        var current_task = document.querySelectorAll(".delete");
+        for(var i=0; i<current_task.length; i++){
+            current_task[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var task = document.querySelectorAll('.task');
+        for(var i =0; i<task.length; i++){
+            task[i].onclick = function(){
+                this.classList,toggle('completed');
+            }
+        }
+
+        document.querySelector("#newtask input").value = "";
+    }
+}
